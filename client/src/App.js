9e7f668283be16a00ef589f9b1a3e0f96c63a886
Nafiz Al din <siriusal121211@gmail.com>
2020-10-23 22:27:17 +0600

    some basic layouts been set

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import './App.css'
 import Navbar from './components/layouts/Navbar'
+import Landing from './components/layouts/Landing'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
 
 
 const App = () => {
     return (
-        <div>
-            <Navbar/>
-        </div>
+        <BrowserRouter>
+            <div>
+                <Navbar/>
+                <Route exact path='/' component={Landing}/>
+                <div className="container">
+                    <Switch>
+                        <Route path='/register' component={Register}/>
+                        <Route path='/login' component={Login}/>
+                    </Switch>
+                </div>
+            </div>
+        </BrowserRouter>
     );
 }
 
